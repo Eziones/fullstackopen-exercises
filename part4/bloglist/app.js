060687d5af6d6b15f9e8a0de282c657b699baeee
Eziones <romaincastro@gmail.com>
2025-09-08 22:29:37 +0200

    moved the app in the right file and exported routes to controller

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,1 +1,44 @@
-const blogsRouter = require("./controllers/blogs")
+const express = require('express')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.set('strictQuery', false)
+mongoose.connect(config.MONGODB_URI)
+  .then(result => {
+    console.log('connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('an error occured:', error.message)
+  })
+
+app.use(express.static('dist'))
+app.use(express.json())
+// requestLogger
+
+app.use('/api/blogs', blogsRouter)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: "malformatted id" })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,43 +1,7 @@
-const express = require('express')
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+const logger = require('./utils/logger')
 
-const app = express()
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: "malformatted id" })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-app.use(errorHandler)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
